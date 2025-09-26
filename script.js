10e153b74c7ccfbe7ec7a41f6ae8f9a168667356
Sushovan Bandopadhyay <chandidas.banerjee1@gmail.com>
2025-09-26 17:37:12 +0530

    Update and rename n script.js to script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Navbar Slide-In
 gsap.to(".navbar", {
-  y: 80,  // slide in from top
+  y: 80,
   duration: 1,
   ease: "power3.out"
 });
 
-// Button Hover Pulse (JS-based)
 document.querySelectorAll(".btn").forEach(button => {
   button.addEventListener("mouseenter", () => {
     gsap.to(button, { scale: 1.1, duration: 0.3, ease: "power1.inOut" });
@@ -15,7 +13,6 @@ document.querySelectorAll(".btn").forEach(button => {
   });
 });
 
-// Scroll-Triggered Fade-Ins
 gsap.utils.toArray(".content").forEach(section => {
   gsap.from(section, {
     opacity: 0,
